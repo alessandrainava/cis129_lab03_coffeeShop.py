b1a63df61f7af9e1f6e88515d5358d5c2a2b182f
alessandrainava <159486119+alessandrainava@users.noreply.github.com>
2024-04-16 14:20:43 -0700

    Update cis129_lab03_coffeeshop.py
    I've added two items to the menu. Croissants and tea as well as a thank you message to leave the customer off on a good note.

--- cis129_lab03_coffeeshop.py
+++ cis129_lab03_coffeeshop.py
@@ -5,6 +5,10 @@ def main():
     # get input from customer
     print("Number of coffees bought?")
     nbrofcoffees = int(input())
+    print("Number of croissants bought?")
+    nbrofcroissants = int(input())
+    print("Number of teas bought?")
+    nbrofteas = int(input())
     print("Number of muffins bought?")
     nbrofmuffins = int(input())
     print ('*' * 39) 
@@ -13,12 +17,16 @@ def main():
     # local variables
     priceofcoffee = 5
     priceofmuffins = 4
+    priceofcroissants = 3
+    priceofteas = 4
     tax = float(0.06)
-    subtotal = (nbrofcoffees * float(priceofcoffee)) + (nbrofmuffins * float(priceofmuffins))
+    subtotal = (nbrofcoffees * float(priceofcoffee)) + (nbrofmuffins * float(priceofmuffins)) + (nbrofcroissants * float(priceofcroissants)) + (nbrofteas * float(priceofteas))
     taxCost = subtotal * tax 
     totalCost = subtotal + taxCost
     totalCoffee = nbrofcoffees * priceofcoffee
     totalMuffin = nbrofmuffins * priceofmuffins
+    totalCroissants = nbrofcroissants * priceofcroissants
+    totalTea = nbrofteas * priceofteas
     
     # print receipt
 
@@ -28,11 +36,16 @@ def main():
         print(f'{nbrofcoffees} Coffee at ${priceofcoffee:.2f} each: ${totalCoffee:.2f}')
     if nbrofmuffins >= 1:
         print(f'{nbrofmuffins} Muffins at ${priceofmuffins:.2f} each: ${totalMuffin:.2f}')
+    if nbrofcroissants >= 1:
+        print(f'{nbrofcroissants} Croissants at ${priceofcroissants:.2f} each: ${totalCroissants:.2f}')
+    if nbrofteas >= 1:
+        print(f'{nbrofteas} Teas at ${priceofteas:.2f} each: ${totalTea:.2f}')
     print(f'6% tax: ${taxCost}')
     print('-' * 9)
     print(f'Total: $ {totalCost}')
     print('*' * 39)
-    
+    print()
+    print("Thank you! Enjoy your sweet treats!")
 #display the receipt to the customer
     
 
